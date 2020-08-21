542ceedb67057fe1a2c94ac439cf02bcbacf4612
Kelin Field <kfield111@hotmail.com>
2020-08-21 01:08:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_reader :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+def match (array)
+  @anagram_results = []
+  array.each do |words|
+    if (word.each_char.sort == words.each_char.sort)
+      @anagram_results << words
+    end
+  end
+  @anagram_results
+end
+
+
+end
