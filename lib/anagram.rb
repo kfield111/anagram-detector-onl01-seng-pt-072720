4fecadc9c2c381b4502e28e7e6d4916834ed225c
Kelin Field <kfield111@hotmail.com>
2020-08-21 00:53:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
 
 def match (array)
   array.detect do |words|
-    if (words.sort == word.sort)
+    if (words.each_char.sort == word.each_char.sort)
       @@anagram_results << words
     else
       return []
