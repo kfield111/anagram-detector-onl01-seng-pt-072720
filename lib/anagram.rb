d87d03d07ee43e4ff4682f6353cb5249b8f13868
Kelin Field <kfield111@hotmail.com>
2020-08-21 01:02:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,22 +2,22 @@ require 'pry'
 class Anagram
   attr_reader :word
 
-@@anagram_results = []
 
   def initialize(word)
     @word = word
   end
 
 def match (array)
+  anagram_results
   array.detect do |words|
     if (words.each_char.sort == word.each_char.sort)
-      @@anagram_results << words
+      anagram_results << words
       binding.pry
     else
       return []
     end
   end
-  @@anagram_results
+  anagram_results
 end
 
 
