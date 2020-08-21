800610391ff1190ea4868dc0146f9323a6b92760
Kelin Field <kfield111@hotmail.com>
2020-08-21 01:06:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,6 @@ def match (array)
   array.each do |words|
     if (word.each_char.sort == words.each_char.sort)
       anagram_results << words
-      binding.pry
     else
       return []
     end
