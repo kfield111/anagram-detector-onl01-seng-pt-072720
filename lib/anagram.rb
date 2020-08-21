dcc1f985bd86e9640e085e17fe4d7ea74db2b7de
Kelin Field <kfield111@hotmail.com>
2020-08-21 01:05:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
 def match (array)
   anagram_results = []
   array.detect do |words|
-    if (words.each_char.sort == word.each_char.sort)
+    if (word.each_char.sort == words.each_char.sort)
       anagram_results << words
       binding.pry
     else
